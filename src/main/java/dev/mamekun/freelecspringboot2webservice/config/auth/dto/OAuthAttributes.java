67fda62180b61e6c10c2725c38717a3cebc70047
Heo Sangmin <heosangmin@gmail.com>
2022-09-02 00:15:52 +0900

    add 5.5

--- src/main/java/dev/mamekun/freelecspringboot2webservice/config/auth/dto/OAuthAttributes.java
+++ src/main/java/dev/mamekun/freelecspringboot2webservice/config/auth/dto/OAuthAttributes.java
@@ -34,17 +34,42 @@ public class OAuthAttributes {
             String registrationId,
             String userNameAttributeName,
             Map<String, Object> attributes) {
+
+        if ("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
     private static OAuthAttributes ofGoogle(
             String userNameAttributeName,
-            Map<String, Object> attributes) {
+            Map<String, Object> attributes
+    ) {
         return OAuthAttributes.builder()
                 .name((String) attributes.get("name"))
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(
+        String userNameAttributeName,
+        Map<String, Object> attributes
+    ) {
+        System.out.printf("[DEBUG] name: %s, email: %s, profile_image: %s%n",
+                (String) attributes.get("name"),
+                (String) attributes.get("email"),
+                (String) attributes.get("profile_image"));
+
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
